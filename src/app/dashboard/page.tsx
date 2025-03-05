e77d19027ff2a144646a2d7c25c7d593c7a7f0c6
Kabir Ahmad <kabirdev.777@gmail.com>
2025-03-05 16:35:55 +0500

    created message component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+// import MessageCard from '@/components/MessageCard'
+
 
 const Dashboard = () => {
   return (
-    <div>page</div>
+    <div>
+      {/* <MessageCard /> */}
+    </div>
   )
 }
 
